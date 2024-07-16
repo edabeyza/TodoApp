848e9f4914dbc5edf3e398de8fc14be214ca3e3f
edabeyza <edabeyzaozeren@gmail.com>
2024-07-16 21:17:32 +0300

    modes done

---
+++ backend/src/models/todo.js
@@ -0,0 +1,41 @@
+"use strict"
+
+const {Schema, model} = require('mongoose')
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3,
+        maxlenght: 200
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3,
+        maxlenght: 200
+    },
+    priority: {
+        type: String,
+        enum: {
+            values: ['low', 'medium', 'high'], 
+            message: 'Invalid priority value'
+        },
+        default: 'medium'
+    },
+    isDone: {
+        type: Boolean,
+        default: false
+    }
+}, 
+{   
+    collection: 'todos',
+    timestamps: true
+})
+
+// Model names supposed to be written with PascalCase
+module.exports = {
+    Todo: model('Todo', todoSchema)
+}
